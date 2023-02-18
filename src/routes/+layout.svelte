de31d01343491558c0c1ecb891a1ce9627a9b80f
te <thrizzle@skiff.com>
2023-02-17 23:38:04 -0500

    ⚡️ improve perf and add rundown section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,30 @@
 <script>
     import "uno.css"
     import "../app.css"
+    import Button from "../components/Button.svelte"
 </script>
-<!-- <nav class="w-screen flex -ml-1.5 fixed backdrop-blur px-10 py-3" >
-    <p class="text-white vertical-middle">fdsafsa</p>
-</nav> -->
-<header class="text-gray-600 body-font fixed w-screen pt-2 backdrop-blur text-white" style="background-color: rgba(0, 0, 0, 0.15);">
+
+<header class="z-100 text-neutral-600 body-font fixed w-screen pt-2 backdrop-blur-xl text-white" style="background-color: rgba(0, 0, 0, 0.15);">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl text-white">Hack Laurel</span>
+    <a class="flex title-font font-medium items-center hover:scale-105 transition ease-out duration-350 text-neutral-900 mb-4 md:mb-0" href="/">
+      <img src="/hacklaurel_wm.webp" class="h-15" alt="Hack Laurel Wordmark" height={15}>
     </a>
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
     </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+    <Button classes="py-2" hover={false}>Register</Button>
   </div>
 </header>
 <slot/>
+<footer class="text-neutral-400 bg-black -mx-2 -my-5.5">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <a class="flex title-font font-medium items-center md:justify-start justify-center text-white" href="/">
+      <img src="/hacklaurel_wm.webp" class="h-10" alt="Hack Laurel Wordmark">
+    </a>
+    <p class="text-4 text-neutral-400 sm:ml-4 sm:pl-4 sm:border-l-2 my-auto sm:border-neutral-800 sm:py-2 ">© 2023 Hack Laurel —
+      Created by<a href="https://thrzl.xyz" class="text-neutral-400 ml-1" target="_blank" rel="noopener noreferrer">thrzl</a>
+    </p>
+    <span class="sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      Fiscally sponsored by <span class="text-primary font-bold">The Hack Foundation</span>.<br>Nonprofit EIN: <span class="text-primary font-bold">81-2908499</span>.
+    </span>
+  </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,55 @@
+<script>
+    import Button from "../components/Button.svelte";
+</script>
+<svelte:head>
+<title>Hack Laurel — Unleash Your Creativity</title>
+<meta name="title" content="Hack Laurel — Unleash Your Creativity">
+<meta name="description" content="Hack Laurel is a high school hackathon where students from PG come together to turn their ideas into reality. ">
+
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://hacklaurel.net/">
+<meta property="og:title" content="Hack Laurel — Unleash Your Creativity">
+<meta property="og:description" content="Hack Laurel is a high school hackathon where students from PG come together to turn their ideas into reality. ">
+<meta property="og:image" content="https://hacklaurel.net/hacklaurel_og.png">
+
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://hacklaurel.net/">
+<meta property="twitter:title" content="Hack Laurel — Unleash Your Creativity">
+<meta property="twitter:description" content="Hack Laurel is a high school hackathon where students from PG come together to turn their ideas into reality. ">
+<meta property="twitter:image" content="https://hacklaurel.net/hacklaurel_og.png">    
+</svelte:head>
+
 <div class="h-102vh w-screen flex -mx-2 -my-2 p-0 bg-cover bg-filter-brightness-50 bg-no-repeat" style="background: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.775), rgba(0, 0, 0, 1)), url(./hero.webp); background-position: center; background-size: cover">
     <div class="m-auto w-max-content h-max-content max-w-1/2 text-white text-center">
         <h1 class="text-7xl font-800 mb-0">Unleash your creativity.</h1>
-        <p class="text-2xl mb-10 font-500">Unleash your creativity and problem-solving skills at Hack Laurel, where PGCPS students come together to design and build innovative solutions to real-world challenges.</p>
-        <a href="https://hacklaurel.com" class="group font-800 text-2xl mt-10 p-5 rounded-full transition duration-500 bg-primary hover:scale-75 text-white decoration-none">Register <span
-									class="inline-block leading-tight font-normal group-hover:translate-x-1.5 transition duration-250 ease-out"
-									>→</span
-								></a>
+        <p class="text-2xl font-500">Unleash your creativity and problem-solving skills at Hack Laurel, where PGCPS students come together to design and build innovative solutions to real-world challenges.</p>
+        <div class="backdrop-blur p-5 b-2 mb-10 b-white rounded-xl" style="background: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))">
+            <h1 class="text-4xl font-800 mb-0 -spacing-2">Coming Soon.</h1>
+            <p class="text-2xl font-500"><span class="font-800">Hack Laurel</span> will be held on <span class="font-800">May 20th, 2023</span>.</p>
+        </div>
+        <Button href="https://hacklaurel.com">Register</Button>
     </div>
 </div>
-<div class="h-screen w-screen flex -mx-2 -my-5.5 bg-black" style="background: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.775), rgba(0, 0, 0, 0.75)), url(./rundown-epoch.webp); background-position: center; background-size: cover">
-    <div class="m-auto w-max-content h-max-content max-w-1/2 text-white mt-50 text-center">
+<div class="w-screen h-full flex -mx-2 -my-5.5 bg-black" style="background: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.775), rgba(0, 0, 0, 0.75)), url(./rundown-epoch.webp); background-position: center; background-size: cover">
+    <div class="m-auto w-max-content h-max-content max-w-1/2 text-white text-center">
         <h1 class="text-7xl font-800">The Rundown.</h1>
-        <p>Hack Laurel is a high school hackathon where students from PG come together to turn their ideas into reality. Join us for a weekend of coding, creating, and problem-solving as we work together to build innovative solutions to real-world challenges involving students.</p>
+        <p class="text-2xl">Hack Laurel is a high school hackathon where students from PG come together to turn their ideas into reality. Join us for a weekend of coding, creating, and problem-solving as we work together to build innovative solutions to real-world challenges involving students.<br/><br/>
+
+        During this event, you'll have the opportunity to learn new skills, connect with like-minded individuals, and expand your network. Whether you're a beginner or an experienced programmer, Hack Laurel welcomes participants of all levels to collaborate and innovate in a supportive and inclusive environment.<br/>
+        </p>
     </div>
+    <div class="grid mr-35 max-w-1/3 text-neutral ">
+        <div class="backdrop-blur p-5 px-10 b-2 mb-10 b-white rounded-xl hover:text-white transition duration-500" style="background: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))">
+            <h1 class="text-4xl font-800 mb-0 -spacing-2">HACKING?!?!?</h1>
+            <p class="text-2xl font-500">Woah! We aren't hacking <em>servers</em>, we're <em>creating</em>-- think apps and websites. It's completely supervised and educational.</p>
+        </div>
+        <div class="backdrop-blur p-5 px-10 b-2 mb-10 b-white rounded-xl hover:text-white transition duration-500" style="background: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))">
+            <h1 class="text-4xl font-800 mb-0 -spacing-2">I don't know how to code!</h1>
+            <p class="text-2xl font-500">Don't worry, you're not alone! There'll be plenty of time to learn at the event while you're making or in one of our workshops!</p>
+        </div>
+        <div class="backdrop-blur p-5 px-10 b-2 mb-10 b-white rounded-xl hover:text-white transition duration-500" style="background: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))">
+            <h1 class="text-4xl font-800 mb-0 -spacing-2">Who can join?</h1>
+            <p class="text-2xl font-500"><span class="font-800">Any student 18 & under is able to register for the hackathon! We hope to see you there! 👋🏽</p>
+        </div>
+    </div> 
 </div>
